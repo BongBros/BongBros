a7bc40b24e35b3a49f1852edcfa400091cd1474e
Mateusz Kulpa <m.c.kulpa@gmail.com>
2017-02-26 13:58:05 +0100

    first commit

---
+++ Assets/Init.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init : MonoBehaviour {
+
+    public GameObject bong;
+    public GameObject bros;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Renderer>().material.color = Color.black;
+        bong.SetActive(false);
+        bros.SetActive(false);
+
+        Invoke("ShowLogo", 8);
+    }
+
+    void ShowLogo()
+    {
+        bong.SetActive(true);
+        bros.SetActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
